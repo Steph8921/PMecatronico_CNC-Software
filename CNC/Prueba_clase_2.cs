e153daebcc059656a1aee70353a76649146f95eb
Gibran <Gibran@DESKTOP-5GJ5GAP>
2019-08-20 14:23:06 -0500

    Otra vez yo

---
+++ CNC/Prueba_clase_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+        int prueba = false;
+        if (prueba == true)
+        {
+            pass;
+        }
+        else
+        {
+            System.Console.Out.ToString("Destruccion mundial");
+        }
+    }
+}
